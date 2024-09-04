63f6f3faf3079caeb18243f20ae667bd6bd7ba1f
Vihanga Wimalaweera <128669551+dazai2003@users.noreply.github.com>
2024-09-05 02:33:59 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,26 @@
+namespace My_First_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            MessageBox.Show("Your Name is " + text);
+        }
+    }
+}
